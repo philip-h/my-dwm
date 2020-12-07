ec4b8b2d576a936e043bc17ac6a9cf2fc0e8e176
Philip H <philip.habib6347@gmail.com>
2020-12-06 21:28:31 -0500

    Changed dmenu command for dmenu patch high priority

--- config.def.h
+++ config.def.h
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-hp", "chromium,discord,firefox,vlc", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
